36851386d73ad2707d30cc71aaf57b4a9bdaa5ed
ainoapal <ainoapal@gmail.com>
2022-10-21 22:07:55 +0200

    damerau-levemsthein restricted

--- distancias.py
+++ distancias.py
@@ -111,18 +111,69 @@ def levenshtein_cota_optimista(x, y, threshold):
 
 def damerau_restricted_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein restringida con matriz
-    lenX, lenY = len(x), len(y)
     # COMPLETAR
+    #----------------------------------------
+    #     REVISAR ESTA PARTE
+    # ---------------------------------------
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1))
+    for i in range(1, lenX + 1):
+        D[i, 0] = i
+    for j in range(1, lenY + 1):
+        D[0, j] = j
+    for i in range(1, lenX + 1):
+        for j in range(1, lenY + 1):
+            if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]:
+                if x[i - 1] == y[j - 1]:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1], D[i-2][j-2] + 1)
+                else:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1] + 1, D[i-2][j-2] + 1)
+            else:
+                if x[i - 1] == y[j - 1]:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1])
+                else:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1] + 1)
     return D[lenX, lenY]
+    
+
 
 def damerau_restricted_edicion(x, y, threshold=None):
     # partiendo de damerau_restricted_matriz añadir recuperar
     # secuencia de operaciones de edición
     return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
 
-def damerau_restricted(x, y, threshold=None):
+def damerau_restricted(x, y, threshold):
     # versión con reducción coste espacial y parada por threshold
-     return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
+     # COMPLETAR Y REEMPLAZAR ESTA PARTE
+     #----------------------------------------
+    #     REVISAR ESTA PARTE
+    # ---------------------------------------
+    lenX, lenY = len(x), len(y)
+    D = np.ones((lenX + 1, lenY + 1))*np.inf
+    for i in range(0, lenX + 1):
+        D[i, 0] = i
+    for j in range(0, len(y) + 1):
+        D[0, j] = j
+    d = lenX/lenY
+    for i in range(1, lenX + 1):
+        colMin = np.inf
+        for j in range(max(math.floor(d*i-threshold), 1), min(math.ceil(d*i+threshold), lenY + 1)):
+            if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]:
+                if x[i - 1] == y[j - 1]:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1], D[i-2][j-2] + 1)
+                else:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1] + 1, D[i-2][j-2] + 1)
+            else:
+                if x[i - 1] == y[j - 1]:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1])
+                else:
+                    D[i, j] = min(D[i - 1, j] + 1, D[i, j - 1] + 1, D[i-1][j-1] + 1)
+            if colMin > D[i,j]:
+                colMin = D[i,j]
+        if colMin > threshold:
+            return None
+    return D[lenX, lenY]
+
 
 def damerau_intermediate_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein intermedia con matriz
